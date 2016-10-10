4e31ffdf521f36151d6904f2fb524bc8bdd865dd
flegald <flegal.david@gmail.com>
2016-10-10 14:14:41 -0700

    changed static root for heroku

--- MHAPI/MHAPI/settings.py
+++ MHAPI/MHAPI/settings.py
@@ -15,7 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 STATIC_URL = '/static/'
 
